6155f081f02a5ef77fac6b365f65681610551d07
zhangqiang <348043793@qq.com>
2023-07-25 19:21:00 +0800

    0.1.3

---
+++ LibEx/Classes/TestClass.swift
@@ -0,0 +1,14 @@
+//
+//  TestClass.swift
+//  LibEx
+//
+//  Created by 张强 on 2023/7/25.
+//
+
+import UIKit
+
+public class TestClass: NSObject {
+    public func genrate() {
+        debugPrint("你说啥")
+    }
+}
